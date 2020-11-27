180e12a1f1ac8a2a9cb617562a464a40d1ae12f5
VladislavVB <burdukovskij.vlad@mail.ru>
2020-11-27 03:12:45 +0300

    add desctop

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,38 @@
-$(function(){
-    $('.load-more').on('click', function(){
-        const btn = $(this);
-        const loader = btn.find('span');
-        $ajax({
-            url: '/more.html',
-            type: 'GET',
-            beforeSend: function(){
-                btn.attr('disabled', true);
-                loader.addClass('d-inline-block');
-            },
-            success: function(responce){
-                setTimeout(function(){
-                    loader.removeClass('d-inline-block')
-                    btn.attr('disabled', false);
-                    console.log(responce);
-                }, 500);
-            },
-            error: function(){
-                console.error('error!');
-                
-            }
-        });
-    });
-});
+// $(function(){
+//     $('.load-more').on('click', function(){
+//         const btn = $(this);
+//         const loader = btn.find('span');
+//         $ajax({
+//             url: '/more.html',
+//             type: 'GET',
+//             beforeSend: function(){
+//                 btn.attr('disabled', true);
+//                 loader.addClass('d-inline-block');
+//             },
+//             success: function(responce){
+//                 setTimeout(function(){
+//                     loader.removeClass('d-inline-block')
+//                     btn.attr('disabled', false);
+//                     console.log(responce);
+//                 }, 500);
+//             },
+//             error: function(){
+//                 console.error('error!');
+
+//             }
+//         });
+//     });
+// });
+
+
+let swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+// $('.carousel').carousel({
+//     interval: 9000000000
+// })
